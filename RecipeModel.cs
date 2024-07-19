8c5f0b93c7f99fd6ee371f495da0c1609d9fbaeb
Raven1123 <164591968+Raven1123@users.noreply.github.com>
2024-07-19 21:41:31 +0800

    Update RecipeModel.cs

--- RecipeModel.cs
+++ RecipeModel.cs
@@ -1,14 +1,10 @@
-namespace RecipeManagementModels
+﻿namespace RecipeManagement.Models
 {
     public class Recipe
     {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public List<string> Ingredients { get; set; }
-        public string Instructions { get; set; }
-        public string Author { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+        public string Name;
+        public string Description;
+       
+      
     }
-}
+ }
