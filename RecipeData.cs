cf21bb960e0f74a34df4554c34b0b5be52a49bda
Raven1123 <164591968+Raven1123@users.noreply.github.com>
2024-07-19 21:40:23 +0800

    Update RecipeData.cs

--- RecipeData.cs
+++ RecipeData.cs
@@ -1,32 +1,94 @@
-using RecipeManagementModels;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using RecipeManagement.Models;
 
-namespace RecipeManagementData
+namespace RecipeManagement.Data
 {
     public class RecipeData
     {
-        private string connectionString = "YourConnectionString";
+        string _connectionString = "Data Source=DESKTOP-N5OCNK2\\SQLEXPRESS;Initial Catalog=FoodRecipe;Integrated Security=True;";
 
         public List<Recipe> GetRecipes()
         {
-            return new List<Recipe>();
+            List<Recipe> recipes = new List<Recipe>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "SELECT Name, Description FROM Recipes";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Recipe recipe = new Recipe
+                    {
+                        Name = reader["Name"].ToString(),
+                        Description = reader["Description"].ToString()
+                    };
+                   
+                    recipes.Add(recipe);
+                }
+                connection.Close();
+            }
+
+            return recipes;
         }
 
         public int AddRecipe(Recipe recipe)
         {
-            return 1;
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "INSERT INTO Recipes (Name, Description) VALUES (@Name, @Description)";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", recipe.Name);
+                command.Parameters.AddWithValue("@Description", recipe.Description);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected;
+            }
         }
 
         public int UpdateRecipe(Recipe recipe)
         {
-            return 1;
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "UPDATE Recipes SET Description = @Description WHERE Name = @Name";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", recipe.Name);
+                command.Parameters.AddWithValue("@Description", recipe.Description);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected;
+            }
         }
 
-        public int DeleteRecipe(int recipeId)
+        public int DeleteRecipe(string recipeName)
         {
-            return 1;
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = "DELETE FROM Recipes WHERE Name = @Name";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Name", recipeName);
+
+                connection.Open();
+                int rowsAffected = command.ExecuteNonQuery();
+                connection.Close();
+
+                return rowsAffected;
+            }
         }
+
     }
 }
